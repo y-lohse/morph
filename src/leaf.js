5e9fb1bde341585a9ebdfd81e6817f4ff8fa977f
Yannick Lohse <contact@yannick-lohse.fr>
2019-06-27 19:19:56 +0200

    cool leaves

--- src/leaf.js
+++ src/leaf.js
@@ -3,37 +3,54 @@ import random from "lodash/random";
 import scene from "./init";
 import bezier from "./bezier";
 
-const drawLeaf = ({ x, y, length }) => {
+const drawLeaf = ({ x, y, length, width }) => {
+  const segments = random(1, 5);
+  const yStep = length / segments;
   const top = y + length;
-  // const xOffset = random(length * .2, length);
-  // const yOffset = random(-length * .8, length * .8);
-  const xOffset = 80;
-  const yOffset = 20;
+  const xOffset = random(length * .2, length);
+  const yOffset = random(-yStep * .8, yStep * .8);
+  const xInnerOffset = random(width * .1, width - xOffset);
+  // const xOffset = 80;
+  // const yOffset = 0;
 
-  const halfTop = y + length / 2;
-  const halfX = xOffset * .4;
+
+  const path = [];
+  const path2 = [];
+
+  for (let i = 0; i < segments; i++) {
+    const s = (i + 1) / segments;
+    const xOffsetMultiplier = Math.sin(3.14 * s);
+    const xOff = Math.round(xInnerOffset * xOffsetMultiplier);
+
+    const yStep = length / segments;
+    const xStep = 30;
+    const startY = y + yStep * i;
+    const endY = startY + yStep;
+
+    path.push(bezier(x, startY, x + xOff, endY, xOffset, yOffset))
+    path2.push(bezier(x, startY, x - xOff, endY, -xOffset, yOffset))
+  }
 
   const gradient = scene.gradient('linear', function(stop) {
     stop.at(0, '#66C071')
     stop.at(.5, '#259A6A')
     stop.at(.1, '#146B54')
     stop.at(1, '#1E8762')
-  })
+  });
 
   scene
     .path(`
       M ${x} ${y}
-      ${bezier(x, y, halfX, halfTop, xOffset, yOffset)}
-      ${bezier(x, halfTop, x, top, xOffset, yOffset)}
-      ${bezier(x, top, -halfX, halfTop, -xOffset, yOffset)}
-      ${bezier(-halfX, halfTop, x, y, -xOffset, yOffset)}
+      ${path.join(' ')}
+      M ${x} ${y}
+      ${path2.join(' ')}
     `)
-    // .stroke({ color: "#0E5C22", width: 1, linecap: "round" })
     .fill(gradient);
 };
 
 drawLeaf({
   x: 0,
   y: 20,
-  length: 300
+  length: 300,
+  width: 200,
 });
