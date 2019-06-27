39b35a6e8f4c62ea3d5c5de5a8c869413db4bb07
Yannick Lohse <contact@yannick-lohse.fr>
2019-06-27 22:42:22 +0200

    simplified code

--- src/leaf.js
+++ src/leaf.js
@@ -8,34 +8,32 @@ const drawLeaf = ({ x, y, length, width }) => {
   // const segments = random(1, 5);
   // const xOffset = random(width * .2, width);
   // const yOffset = random(-yStep * .8, yStep * .8);
-  // const xInnerOffset = random(width * .1, width);
-  const segments = 1;
+  const segments = 2;
   const xOffset = 250;
   const yOffset = 0;
-  const xInnerOffset = 80;
 
   const yStep = length / segments;
-  const top = y + length;
 
-  const path = [];
-  const path2 = [];
+  const leftHand = [];
+  const rightHand = [];
+
+  const sinusoidal = x => Math.sin(3.14 * x);
 
   for (let i = 0; i < segments; i++) {
-    const s = (i + 1) / segments;
-    const xOffsetMultiplier = Math.sin(3.14 * s);
-    const xOff = Math.round(xInnerOffset * xOffsetMultiplier);
+    const percent = (i + 1) / segments;
+    const widthMultiplier = sinusoidal(percent);
+    const stepWidth = Math.round(width * widthMultiplier);
 
-    const yStep = length / segments;
-    const xStep = 30;
     const startY = y + yStep * i;
     const endY = startY + yStep;
 
-    path.push(bezier(x, startY, x + xOff, endY, xOffset, yOffset))
-    path2.push(bezier(x, startY, x - xOff, endY, -xOffset, yOffset))
+    leftHand.push(bezier(x, startY, x + stepWidth, endY, xOffset, yOffset))
+    rightHand.push(bezier(x, startY, x - stepWidth, endY, -xOffset, yOffset))
   }
 
   const palette = ['#289B61', '#1C5438', '#71BC98', '#56A37E', '#EAC041', '#F9BB00', '#82453E', '#7C3B78', '#46B1C9', '#5A464C'];
-  const baseColor = Color(palette[random(0, palette.length - 1)]);
+  // const baseColor = Color(palette[random(0, palette.length - 1)]);
+  const baseColor = Color(palette[0]);
   const light = baseColor.lighten(.2)
   const dark = baseColor.darken(.2)
 
@@ -46,44 +44,44 @@ const drawLeaf = ({ x, y, length, width }) => {
     stop.at(1, dark.saturate(.3).hex())
   });
 
-  const gradient2 = scene.gradient('linear');
-  gradient2.at(0, '#ffffff', 0);
-
-  const stepSize = 1;
-  const stepDistance = 10;
-
-  for (let i = stepDistance; i < 100; i+=stepDistance){
-    gradient2.at((i - stepSize) / 100, '#ffffff', 0);
-    gradient2.at(i / 100, '#000', .15);
-    gradient2.at((i + stepSize) / 100, '#ffffff', 0);
-  }
-  
-  gradient2.at(1, '#ffffff', 0);
-
-  gradient2.from(0, 0).to(0, 1);
-
   scene
-    .path(`
-      M ${x} ${y}
-      ${path.join(' ')}
-      M ${x} ${y}
-      ${path2.join(' ')}
-    `)
-    .fill(gradient);
+  .path(`
+    M ${x} ${y}
+    ${leftHand.join(' ')}
+    M ${x} ${y}
+    ${rightHand.join(' ')}
+  `)
+  .fill(gradient);
 
-  scene
-    .path(`
-      M ${x} ${y}
-      ${path.join(' ')}
-      M ${x} ${y}
-      ${path2.join(' ')}
-    `)
-    .fill(gradient2);
+  // const gradient2 = scene.gradient('linear');
+  // gradient2.at(0, '#ffffff', 0);
+  //
+  // const stepSize = 1;
+  // const stepDistance = 10;
+  //
+  // for (let i = stepDistance; i < 100; i+=stepDistance){
+  //   gradient2.at((i - stepSize) / 100, '#ffffff', 0);
+  //   gradient2.at(i / 100, '#000', .15);
+  //   gradient2.at((i + stepSize) / 100, '#ffffff', 0);
+  // }
+  //
+  // gradient2.at(1, '#ffffff', 0);
+  //
+  // gradient2.from(0, 0).to(0, 1);
+  //
+  // scene
+  //   .path(`
+  //     M ${x} ${y}
+  //     ${path.join(' ')}
+  //     M ${x} ${y}
+  //     ${rightHand.join(' ')}
+  //   `)
+  //   .fill(gradient2);
 };
 
 drawLeaf({
   x: 0,
   y: 0,
   length: 400,
-  width: 200,
+  width: 100,
 });
