ec03a01b4cef14cf7ec22551961bef147676b8fd
Yannick Lohse <contact@yannick-lohse.fr>
2019-06-27 21:55:21 +0200

    single color gradient

--- src/leaf.js
+++ src/leaf.js
@@ -5,15 +5,17 @@ import scene from "./init";
 import bezier from "./bezier";
 
 const drawLeaf = ({ x, y, length, width }) => {
-  const segments = random(1, 5);
+  // const segments = random(1, 5);
+  // const xOffset = random(width * .2, width);
+  // const yOffset = random(-yStep * .8, yStep * .8);
+  // const xInnerOffset = random(width * .1, width);
+  const segments = 1;
+  const xOffset = 250;
+  const yOffset = 0;
+  const xInnerOffset = 80;
+
   const yStep = length / segments;
   const top = y + length;
-  const xOffset = random(width * .2, width);
-  const yOffset = random(-yStep * .8, yStep * .8);
-  const xInnerOffset = random(width * .1, width);
-  // const xOffset = 150;
-  // const yOffset = 0;
-  // const xInnerOffset = 80;
 
   const path = [];
   const path2 = [];
@@ -32,22 +34,15 @@ const drawLeaf = ({ x, y, length, width }) => {
     path2.push(bezier(x, startY, x - xOff, endY, -xOffset, yOffset))
   }
 
-  const palette = [
-    ['#289B61', '#1C5438'],
-    ['#71BC98', '#56A37E'],
-    ['#66C071', '#146B54'],
-    ['#EAC041', '#F9BB00'],
-    ['#82453E', '#82453E'],
-  ];
-  const colors = palette[random(0, palette.length - 1)];
-  const light = Color(colors[0]);
-  const dark = Color(colors[1]);
+  const palette = ['#289B61', '#1C5438', '#71BC98', '#56A37E', '#EAC041', '#F9BB00', '#82453E', '#7C3B78', '#46B1C9', '#5A464C'];
+  const light = baseColor.lighten(.2)
+  const dark = baseColor.darken(.2)
 
   const gradient = scene.gradient('linear', function(stop) {
     stop.at(0, light.hex())
-    stop.at(.5, light.desaturate(random(.2, .4)).rotate(random(4, 17)).hex())
+    stop.at(.5, light.desaturate(.3).hex())
     stop.at(.51, dark.hex())
-    stop.at(1, dark.lighten(random(.2, .5)).hex())
+    stop.at(1, dark.saturate(.3).hex())
   });
 
   scene
